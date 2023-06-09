1290823628d1b2c7c9dcb3c084cd941d349fc0d5
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-06-09 21:10:53 +0100

    fix linter error

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -3,7 +3,6 @@ class Datum < ApplicationRecord
   has_many :category_data, dependent: :destroy, class_name: 'CategoryDatum'
   has_many :categories, through: :category_data
 
-
   validates :name, presence: true
   validates :amount, numericality: { is_decimal: true }
 end
