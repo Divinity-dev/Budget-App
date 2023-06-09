c1dd52680768332eaca6c53a6c0a073316934c43
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-06-09 22:19:17 +0100

    fix linters errors

--- Gemfile
+++ Gemfile
@@ -56,10 +56,8 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails'
-  gem 'rspec-rails'
   gem 'shoulda-matchers'
 end
-gem 'shoulda-matchers', group: :test
 
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     user = User.new(
-      name: "John Doe",
-      email: "john@example.com",
-      password: "password"
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password'
     )
     expect(user).to be_valid
   end
 
-  it "is not valid without a name" do
+  it 'is not valid without a name' do
     user = User.new(name: nil)
     expect(user).to_not be_valid
   end
